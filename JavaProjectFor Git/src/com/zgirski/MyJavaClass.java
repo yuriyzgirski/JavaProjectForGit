71499e38489413b8a97161d77dcad4b05ad2ea1b
Yuriy N Zgirski <Yuriy N Zgirski@BETTA1.localdomain>
2018-10-23 15:49:24 +0300

    Initial checkin

---
+++ JavaProjectFor Git/src/com/zgirski/MyJavaClass.java
@@ -0,0 +1,11 @@
+package com.zgirski;
+
+public class MyJavaClass {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+	}
+
+}
